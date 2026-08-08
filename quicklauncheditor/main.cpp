59c65c3d4dc2ee15f269585f0d86f80bd7d6ec3b
Clashy13 <s.kiste@web.de>
2026-08-08 18:42:47 +0200

    Changed to qt quick project

--- quicklauncheditor/main.cpp
+++ quicklauncheditor/main.cpp
@@ -1,9 +1,19 @@
-#include <QApplication>
-#include <QMainWindow>
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
 
 int main( int argc, char* argv[] ) {
-    QApplication a( argc, argv );
-    QMainWindow w;
-    w.show();
-    return a.exec();
+    QGuiApplication app( argc, argv );
+
+    QQmlApplicationEngine engine;
+
+    const QUrl url( QStringLiteral( "qrc:/Main.qml" ) );
+    QObject::connect(
+        &engine,
+        &QQmlApplicationEngine::objectCreationFailed,
+        &app,
+        []() { QCoreApplication::exit( -1 ); },
+        Qt::QueuedConnection );
+    engine.load( url );
+
+    return app.exec();
 }
