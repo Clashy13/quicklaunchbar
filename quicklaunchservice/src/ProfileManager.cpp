12d99c90be84bfa711a61212953029cabc722f7e
Clashy13 <s.kiste@web.de>
2026-09-04 20:55:43 +0200

    Enabled reloading of profiles on config changes

--- quicklaunchservice/src/ProfileManager.cpp
+++ quicklaunchservice/src/ProfileManager.cpp
@@ -3,18 +3,28 @@
 #include "serializer/ProfileConfigSerializer.hpp"
 
 #include <QDesktopServices>
-#include <QHotkey>
+#include <QFileSystemWatcher>
 #include <QProcess>
 #include <QUrl>
+#include <memory>
 
 namespace Service {
 
-    ProfileManager::ProfileManager( QObject* parent )
-        : QObject( parent ), _profiles( Serializer::ProfileConfigSerializer::load() ) {
-        for ( auto profile : this->_profiles ) {
-            profile->setParent( this );
-        }
-        this->connectShortcutsToProfiles();
+    ProfileManager::ProfileManager( QObject* parent ) : QObject( parent ) {
+        this->reloadProfiles();
+
+        QFileSystemWatcher* watcher = new QFileSystemWatcher( this );
+        watcher->addPath( Serializer::ProfileConfigSerializer::filePath() );
+
+        connect( watcher,
+                 &QFileSystemWatcher::fileChanged,
+                 this,
+                 [ this, watcher ]( const QString& path ) {
+                     this->reloadProfiles();
+                     if ( !watcher->files().contains( path ) ) {
+                         watcher->addPath( path );
+                     }
+                 } );
     }
 
     void ProfileManager::launchExecutionTargetByIndex( const qsizetype index ) {
@@ -26,12 +36,31 @@ namespace Service {
         }
     }
 
+    void ProfileManager::reloadProfiles() {
+        for ( const auto connection : this->_shortcutConnections ) {
+            this->disconnect( connection );
+        }
+        this->_shortcutConnections.clear();
+
+        this->_hotkeys.clear();
+
+        this->_profiles = Serializer::ProfileConfigSerializer::load();
+        for ( auto profile : this->_profiles ) {
+            profile->setParent( this );
+        }
+        this->connectShortcutsToProfiles();
+    }
+
     void ProfileManager::connectShortcutsToProfiles() {
         for ( const auto profile : this->_profiles ) {
-            auto* hotkey = new QHotkey( QKeySequence( profile->shortcut() ), true, this );
-            connect( hotkey, &QHotkey::activated, this, [ this, profile ]() {
-                emit this->toggleProfile( profile );
-            } );
+            this->_hotkeys.push_back(
+                std::make_unique<QHotkey>( QKeySequence( profile->shortcut() ), true, this ) );
+
+            this->_shortcutConnections.push_back(
+                connect( this->_hotkeys.back().get(),
+                         &QHotkey::activated,
+                         this,
+                         [ this, profile ]() { emit this->toggleProfile( profile ); } ) );
         }
     }
 
